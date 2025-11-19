a2b18febf6f29638533042942f22b0fc71ab8a77
hhdm-institucional-thinkpad <hector.diaz@est.fi.uncoma.edu.ar>
2025-11-19 19:29:10 -0300

    corrijo cargar juego, termino opcion1

--- [IP-TUDW] Trabajo Final-20251115/programaCorsiniDiazM.php
+++ [IP-TUDW] Trabajo Final-20251115/programaCorsiniDiazM.php
@@ -23,20 +23,22 @@ include_once("memoria.php");
 function cargarJuegos (){
     /**Carga 10 o más juegos predefinidos en una colección de juegos
      * @param array $coleccionJuegos, $unJuego, $nombres
-     * @param int $aciertos1, $aciertos2
+     * @param int $aciertos1, $aciertos2, $empates, $total
      * @return $array 
      */
 
-    /* H: Explicación: uso un array de nombres para crear nombres aleatorios, supongo
-    que juegan 5 veces cada juego. Uso un for para cargar la colección de juegos. */
+    /* H: Explicación: uso un array de nombres para crear nombres aleatorios.
+     Uso un for para cargar la colección de juegos. */
     $coleccionJuegos=array();  
     $nombres=array("Sofía", "Alejandro", "María", "Sebastián", "Valentina", "Diego");
 
     for ($i=0; $i < 11; $i++) { 
-        $aciertos1=random_int(0,5);
-        $aciertos2=5-$aciertos1;
+        $total=random_int(2,8); // mínimo 2 juegos y máximo 8, lo humanamente razonable
+        $empates=random_int(0,3); // establesco hasta 3 empates
+        $aciertos1=random_int(0,$total-$empates); // random entre los totales y los ya empatados  
+        $aciertos2=$total-$aciertos1-$empates; // los restantes son aciertos 2
         $unJuego=array("jugador1"=>$nombres[random_int(0,5)], "aciertos1"=>$aciertos1,"jugador2"=>$nombres[random_int(0,5)], "aciertos2"=>$aciertos2);
-        $coleccionJuegos[i]=$unJuego;
+        $coleccionJuegos[$i]=$unJuego;
     }
     return $coleccionJuegos;
 }
@@ -47,15 +49,17 @@ function cargarJuegos (){
 /**************************************/
 
 //Declaración de variables:
-/* @param string $nombre1, $nombre2 
- * @param int $nroJuego
+/*
+ * @param int $cantJuegos, $opcion
+ * @param array $Juegos, $unJuego 
  */
 
 
 //Inicialización de variables:
-$nombre1="";
-$nombre2="";
-
+$opcion=0;
+//Precargado (Punto 11.a)
+$Juegos=cargarJuegos();
+$cantJuegos=count($Juegos); //total de juegos en $Juegos
 
 //Proceso:
 
@@ -69,22 +73,19 @@ echo "jugador 2 " . $juego["jugador2"] . ": " . $juego["aciertos2"] . " aciertos
 
 
 
+
 do {
     $opcion = ...;
 
     
     switch ($opcion) {
         case 1: /* 1) JUGAR A MEMORIA */ 
-            /* Al iniciar se solicitan los nombres de los jugadores
-            * Al finalizar guarda los resultados en una estructura de datos
+            /* Al iniciar se solicitan los nombres de los jugadores (lo hace la funcion en memoria.php)
+            * Al finalizar guarda los resultados en una estructura de datos ($Juegos)
             */
-            echo "Ingrese los nombres de ambos juadores: \nJugador 1: \n";
-            $nombre1=trim(fgets(STDIN));
-            echo "Jugador 2: \n";
-            $nombre2=trim(fgets(STDIN));
-
-            //CONTINUAR...
-
+            $unJuego=jugarMemoria();
+            $Juegos[$cantJuegos]=$unJuego; //Si ya hay 10 juegos, el índice 10 es correcto para guardar el siguiente juego
+            $cantJuegos++;
             break;
 
         case 2: 
